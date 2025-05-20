3ef27aa3a59c53ec4fa4b42e95a7bda1d67a1ddc
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2025-05-20 14:52:27 -0300

    settings page

--- src/app/pages/settings/settings.tsx
+++ src/app/pages/settings/settings.tsx
@@ -1,19 +1,19 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import Header from '~/components/headerButtons/Header/header';
+import { LogOut, ChevronRight } from 'lucide-react-native';
 
 export default function Settings() {
   return (
     <View className="flex-1 bg-white">
-      <Header
-        title="Ajustes"
-        showFilter={false}
-        showBackButton={true}
-      />
+      <Header title="Ajustes" showFilter={false} showBackButton={true} />
       <View className="flex-1 items-center justify-start pt-10">
-        <TouchableOpacity className="h-16 w-11/12 flex-row items-center justify-start rounded-lg bg-black shadow-md">
-          <View className="flex flex-row items-center w-full px-3">
-            <Text className="text-white text-lg font-bold flex-1">Olá</Text>
-            <View className="h-8 w-8 bg-white rounded" />
+        <TouchableOpacity className="h-16 w-11/12 flex-row items-center justify-start rounded-lg shadow-md">
+          <View className="h-[52px] w-[52px] items-center justify-center rounded-lg bg-inputColor">
+            <LogOut color="black" size={20} />
+          </View>
+         <Text className="flex-1 text-lg font-bold text-black px-3">Sair</Text>
+          <View className="items-center justify-center">
+            <ChevronRight color="#7C8BA0" size={20} />
           </View>
         </TouchableOpacity>
       </View>
