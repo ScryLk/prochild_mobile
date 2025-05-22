b9e5e7c095b06877eb1c3aa5590cb168f12cc2ba
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2025-05-22 12:04:20 -0300

    status bar changes

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
 import 'expo-router/entry';
 import { View } from 'react-native';
+import { useEffect } from 'react';
+import { Appearance } from 'react-native';
 
 import './global.css';
 
 export default function App() {
+  useEffect(() => {
+    Appearance.setColorScheme('light'); 
+  }, []);
   return (
      <View>
       <StatusBar style="light" />
